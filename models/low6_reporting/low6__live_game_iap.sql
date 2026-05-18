c31b3baf44f4588f38248e07f8a20361a53d2be1
Will Breeden <will@low6.co.uk>
2026-05-18 13:36:56 +0100

    Live Games Reporting- Update IAP and 7days Alien dash

--- models/low6_reporting/low6__live_game_iap.sql
+++ models/low6_reporting/low6__live_game_iap.sql
@@ -19,7 +19,85 @@ prizekings as (
         on t.user_id = u.user_id
     group by 1
 
+),
+
+sevendays_pq as (
+
+    select
+        '7dpq' as app_id,
+        sum(ticket_price) as gross_revenue,
+        sum(case when session_date = current_date() - 1 then ticket_price else null end) as yesterday_gross_revenue,
+        sum(case when session_date >= current_date() - 8 and session_date < current_date() then ticket_price else null end) as last_7_days_gross_revenue,
+        sum(case when session_date >= current_date() - 29 and session_date < current_date() then ticket_price else null end) as last_28_days_gross_revenue
+    from {{ ref('mart_seven_days__entries') }}
+    where game_name = 'Pirate Quest'
+
+),
+
+sevendays_cd as (
+
+    select
+        '7dcd' as app_id,
+        sum(ticket_price) as gross_revenue,
+        sum(case when session_date = current_date() - 1 then ticket_price else null end) as yesterday_gross_revenue,
+        sum(case when session_date >= current_date() - 8 and session_date < current_date() then ticket_price else null end) as last_7_days_gross_revenue,
+        sum(case when session_date >= current_date() - 29 and session_date < current_date() then ticket_price else null end) as last_28_days_gross_revenue
+    from {{ ref('mart_seven_days__entries') }}
+    where game_name = 'Coin Drop'
+    
+),
+
+sevendays_dz as (
+
+    select
+        '7ddz' as app_id,
+        sum(ticket_price) as gross_revenue,
+        sum(case when session_date = current_date() - 1 then ticket_price else null end) as yesterday_gross_revenue,
+        sum(case when session_date >= current_date() - 8 and session_date < current_date() then ticket_price else null end) as last_7_days_gross_revenue,
+        sum(case when session_date >= current_date() - 29 and session_date < current_date() then ticket_price else null end) as last_28_days_gross_revenue
+    from {{ ref('mart_seven_days__entries') }}
+    where game_name = 'Drop Zone'
+    
+),
+
+sevendays_ad as (
+
+    select
+        '7dad' as app_id,
+        sum(ticket_price) as gross_revenue,
+        sum(case when session_date = current_date() - 1 then ticket_price else null end) as yesterday_gross_revenue,
+        sum(case when session_date >= current_date() - 8 and session_date < current_date() then ticket_price else null end) as last_7_days_gross_revenue,
+        sum(case when session_date >= current_date() - 29 and session_date < current_date() then ticket_price else null end) as last_28_days_gross_revenue
+    from {{ ref('mart_seven_days__entries') }}
+    where game_name = 'Alien Dash'
+    
+),
+
+unioned as (
+
+    select * from prizekings
+
+    union all
+
+    select *
+    from sevendays_pq
+
+    union all
+
+    select *
+    from sevendays_cd
+
+    union all
+
+    select *
+    from sevendays_dz
+
+    union all
+
+    select *
+    from sevendays_ad
+
 )
 
-select * from prizekings
+select * from unioned
 
