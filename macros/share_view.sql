08c6b281a3ba1101ed94b6151e7d691592cf476b
Will Breeden <will@low6.co.uk>
2026-08-24 15:08:01 +0100

    Macros- Updaed share view macro to execute as owner using grant view to share stored procedure

--- macros/share_view.sql
+++ macros/share_view.sql
@@ -1,10 +1,7 @@
 {% macro share_view(view_schema, view_name, share_name) %}
   {% if target.name == "prod" %}
     {% set sql %}
-      -- use role accountadmin;
-      GRANT USAGE ON SCHEMA {{ target.database }}.{{ view_schema }} TO SHARE {{ share_name }};
-      GRANT SELECT ON VIEW {{ target.database }}.{{ view_schema }}.{{ view_name }} TO SHARE {{ share_name }};
-      -- use role {{ target.role }};
+      CALL {{ target.database }}.dbt_admin.grant_view_to_share('{{ view_schema }}', '{{ view_name }}', '{{ share_name }}');
     {% endset %}
     {% set table = run_query(sql) %}
   {% endif %}
