3aa0fa20d24d083964d85103e68ce9cd7265cb77
Will Breeden <will@low6.co.uk>
2026-08-24 15:16:14 +0100

    Macros- Updated create share macros to execute as owner in snowflake

--- macros/create_share.sql
+++ macros/create_share.sql
@@ -1,16 +1,8 @@
--- macros/create_share.sql
 {% macro create_share(share_name, accounts) %}
   {% if target.name == "prod" %}
+    {% set accounts_array = "ARRAY_CONSTRUCT('" ~ accounts | join("','") ~ "')" %}
     {% set sql %}
-      -- use role accountadmin;
-      CREATE SHARE IF NOT EXISTS {{ share_name }};
-      GRANT USAGE ON DATABASE {{ target.database }} TO SHARE {{ share_name }};
-
-      {% for account in accounts %}
-        ALTER SHARE {{ share_name }} ADD ACCOUNTS = {{ account }};
-      {% endfor %}
-
-      -- use role {{ target.role }};
+      CALL {{ target.database }}.dbt_admin.create_share_proc('{{ share_name }}', {{ accounts_array }});
     {% endset %}
     {% set table = run_query(sql) %}
   {% endif %}
